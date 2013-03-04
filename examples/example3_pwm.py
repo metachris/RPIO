b78d20629bb296ce8c380543cc68044391e3113f
Chris Hager <chris@metachris.org>
2013-03-04 14:57:47 +0100

    example update

--- examples/example3_pwm.py
+++ examples/example3_pwm.py
@@ -1,5 +1,6 @@
 import time
 import RPIO.PWM as PWM
+import RPIO.PWM.Notes
 
 GPIO = 17
 PERIOD_TIME_US = 2000  # 2000us
@@ -9,8 +10,16 @@ PULSE_WIDTH_INCREMENTS_US = 10
 # Setup PWM for all channes
 PWM.setup(PULSE_WIDTH_INCREMENTS_US)
 
+# To use PCM instead of PWM for the timings:
+#PWM.setup(PULSE_WIDTH_INCREMENTS_US, delay_hw=PWM.DELAY_VIA_PCM)
+
+
 # Initialize DMA channel 0 for GPIO 17
 PWM.init_channel(0, GPIO, PERIOD_TIME_US)
+
+# Alternatively you can init a channel with a frequency:
+# PWM.init_channel_with_freq(0, GPIO, RPIO.PWM.Notes.A4)
+
 
 # Now set various pulse widths. Note that pulse widths are multiplied
 # by PULSE_WIDTH_INCREMENTS_US to get the pulse time in microseconds.
@@ -20,5 +29,5 @@ time.sleep(10)
 PWM.set_channel_pulse(0, 10)   # 100 us
 time.sleep(10)
 
-PWM.set_channel_pulse(0, 1)    # 1 us
+PWM.set_channel_pulse(0, 1)    # 10 us
 time.sleep(10)
